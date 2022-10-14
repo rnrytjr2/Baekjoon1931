d13e32bf848b708a50912b424e3679b9c8312db2
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-10-15 00:06:05 +0900

    solved

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int N, answer = 1;
+vector<pair<int, int>> meetingtime;
+pair<int, int> lastmeeting;
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		meetingtime.push_back(make_pair(b, a));
+	}
+	sort(meetingtime.begin(), meetingtime.end());
+	lastmeeting = meetingtime[0];
+	for (int i = 1; i < N; i++)
+	{
+		if (meetingtime[i].second >= lastmeeting.first)
+		{
+			answer++;
+			lastmeeting = meetingtime[i];
+		}
+	}
+	cout << answer;
+}
+
